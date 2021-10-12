1a86e360fb3fdf2640e338e21dc6ed1dfeb7290e
Howie21 <bryce9643@gmail.com>
2021-10-12 14:04:28 -0400

    Refactor for Flow Control of main.py

--- Class and Objects WS/ShoppingCart/main.py
+++ Class and Objects WS/ShoppingCart/main.py
@@ -1,11 +1,15 @@
 from customer import Customer
+from product import Product
 
 
 customer1 = Customer("Wes")
 print(customer1.name)
-customer1.place_item_in_cart("Cream Chesse", 3, "Dairy")
-customer1.place_item_in_cart("Bagel Bites", 7, "Bread")
-customer1.place_item_in_cart("Grill", 150, "Appliances")
+product1 = Product("Cream Chesse", 3, "Dairy")
+product2 = Product("Bagel Bites", 7, "Bread")
+product3 = Product("Grill", 150, "Appliances")
+customer1.place_item_in_cart(product1)
+customer1.place_item_in_cart(product2)
+customer1.place_item_in_cart(product3)
 customer1.review_items_in_cart()
 customer1.take_all_items_out()
 customer1.review_items_in_cart()
